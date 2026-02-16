e66633672ab27e1a22e40b75e8ca5cada24b8964
Ben <benpalmer.ref@gmail.com>
2026-02-15 19:21:51 -0500

    remove purposeful failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,7 +10,6 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	return "", errors.New("breaking on purpose")
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
